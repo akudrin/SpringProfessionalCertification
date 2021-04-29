18e394cf13a32565b113febf2f4b37641b5802fe
Andrei Kudrin <andreikudrin@gmail.com>
2021-04-29 13:51:06 +0100

    Udemy Spring Chad Darby - DI Bean Lifecycle prototype

--- Spring and Hibernate for Beginners Udemy/Section 4/spring-demo-one/src/com/akudrin/springdemo/SetterDemoApp.java
+++ Spring and Hibernate for Beginners Udemy/Section 4/spring-demo-one/src/com/akudrin/springdemo/SetterDemoApp.java
@@ -5,7 +5,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class SetterDemoApp {
 
 	public static void main(String[] args) {
-		// load the spring config
+		// load the spring configuration file
 		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 
 		// retrieve bean from spring container
