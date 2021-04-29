ffe90cb3906e3933de301c77403a606c536787d2
Andrei Kudrin <andreikudrin@gmail.com>
2021-04-29 12:41:46 +0100

    Udemy Spring Chad Darby - Dependency Injection Literal Values

--- Spring and Hibernate for Beginners Udemy/Section 4/spring-demo-one/src/com/akudrin/springdemo/CricketCoach.java
+++ Spring and Hibernate for Beginners Udemy/Section 4/spring-demo-one/src/com/akudrin/springdemo/CricketCoach.java
@@ -2,11 +2,32 @@ package com.akudrin.springdemo;
 
 public class CricketCoach implements Coach {
 
+	private String emailAddress;
+	private String team;
+
 	// define a private field for the dependency
 	private FortuneService fortuneService;
 
 	public CricketCoach() {
 		System.out.println("CricketCoach: inside no-arg constructor");
+	}
+
+	public void setEmailAddress(String emailAddress) {
+		System.out.println("CricketCoach: inside setter method setEmailAddress");
+		this.emailAddress = emailAddress;
+	}
+
+	public void setTeam(String team) {
+		System.out.println("CricketCoach: inside setter method setTeam");
+		this.team = team;
+	}
+
+	public String getEmailAddress() {
+		return emailAddress;
+	}
+
+	public String getTeam() {
+		return team;
 	}
 
 	public void setFortuneService(FortuneService fortuneService) {
