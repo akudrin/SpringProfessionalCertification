5dd3fb2da4c7e907160195462e107e40b3878acf
Andrei Kudrin <andreikudrin@gmail.com>
2021-05-12 14:16:18 +0100

    Udemy Spring Chad Darby - Spring config with Java Code no xml

--- Spring and Hibernate for Beginners Udemy/Section 7/src/com/akudrin/springdemo/SwimCoach.java
+++ Spring and Hibernate for Beginners Udemy/Section 7/src/com/akudrin/springdemo/SwimCoach.java
@@ -1,16 +1,23 @@
 package com.akudrin.springdemo;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class SwimCoach implements Coach {
 
-	@Autowired
-	@Qualifier("happyFortuneService")
+	/*
+	 * @Autowired
+	 * 
+	 * @Qualifier("happyFortuneService")
+	 */
+
 	private FortuneService fortuneService;
+
+	public SwimCoach(FortuneService theFortuneService) {
+		fortuneService = theFortuneService;
+
+	}
 
 	@Value("${foo.email}")
 	private String email;
