d0a70eea3b849d802519379715464749dcc2a264
Andrei Kudrin <andreikudrin@gmail.com>
2021-05-12 09:28:13 +0100

    Udemy Spring Chad Darby - Java Annotations -  How to inject properties file using Java annotations

--- Spring and Hibernate for Beginners Udemy/Section 7/src/com/akudrin/springdemo/AnnotationDemoApp.java
+++ Spring and Hibernate for Beginners Udemy/Section 7/src/com/akudrin/springdemo/AnnotationDemoApp.java
@@ -9,8 +9,15 @@ public class AnnotationDemoApp {
 		// read spring config file
 		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 
-		// get the bean from srping container
+		// get the bean from spring container
 		Coach theCoach = context.getBean("tennisCoach", Coach.class);
+		SwimCoach swimCoach = context.getBean("swimCoach", SwimCoach.class);
+
+		System.out.println(swimCoach.getDailyWorkout());
+		System.out.println(swimCoach.getDailyFortune());
+		System.out.println("email: " + swimCoach.getEmail());
+		System.out.println("team: " + swimCoach.getTeam());
+
 		// call a method on the bean
 		System.out.println(theCoach.getDailyWorkout());
 		System.out.println(theCoach.getDailyFortune());
