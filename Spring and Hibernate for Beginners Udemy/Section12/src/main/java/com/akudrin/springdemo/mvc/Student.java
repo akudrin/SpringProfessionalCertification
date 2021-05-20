2385c9296c0c779f09dc03185f7e3162a40fb773
Andrei Kudrin <andreikudrin@gmail.com>
2021-05-20 14:05:54 +0100

    Udemy Spring Chad Darby - Spring MVC - Use properties file to load country options for drop down menu

--- Spring and Hibernate for Beginners Udemy/Section12/src/main/java/com/akudrin/springdemo/mvc/Student.java
+++ Spring and Hibernate for Beginners Udemy/Section12/src/main/java/com/akudrin/springdemo/mvc/Student.java
@@ -1,24 +1,22 @@
 package com.akudrin.springdemo.mvc;
 
-import java.util.LinkedHashMap;
-
 public class Student {
 
 	private String firstName;
 	private String lastName;
 	private String country;
 
-	private LinkedHashMap<String, String> countryOptions;
+	// private LinkedHashMap<String, String> countryOptions;
 
 	public Student() {
 
-		countryOptions = new LinkedHashMap<String, String>();
-
-		countryOptions.put("BR", "Brazil");
-		countryOptions.put("FR", "France");
-		countryOptions.put("DE", "Germany");
-		countryOptions.put("IN", "India");
-		countryOptions.put("US", "United States of America");
+		/*
+		 * countryOptions = new LinkedHashMap<String, String>();
+		 * 
+		 * countryOptions.put("BR", "Brazil"); countryOptions.put("FR", "France");
+		 * countryOptions.put("DE", "Germany"); countryOptions.put("IN", "India");
+		 * countryOptions.put("US", "United States of America");
+		 */
 
 	}
 
@@ -46,8 +44,9 @@ public class Student {
 		this.country = country;
 	}
 
-	public LinkedHashMap<String, String> getCountryOptions() {
-		return countryOptions;
-	}
+	/*
+	 * public LinkedHashMap<String, String> getCountryOptions() { return
+	 * countryOptions; }
+	 */
 
 }
--- Spring and Hibernate for Beginners Udemy/Section12/src/main/java/com/akudrin/springdemo/mvc/StudentController.java
+++ Spring and Hibernate for Beginners Udemy/Section12/src/main/java/com/akudrin/springdemo/mvc/StudentController.java
@@ -1,5 +1,8 @@
 package com.akudrin.springdemo.mvc;
 
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -8,12 +11,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/student")
 public class StudentController {
+
+	@Value("#{countryOptions}")
+	private Map<String, String> countryOptions;
+
 	@RequestMapping("/showForm")
 	public String showForm(Model theModel) {
 		// create new student object
 		Student theStudent = new Student();
 		// add student object to the model
 		theModel.addAttribute("student", theStudent);
+		// add the country options to the model
+		theModel.addAttribute("theCountryOptions", countryOptions);
+
 		return "student-form";
 	}
 
