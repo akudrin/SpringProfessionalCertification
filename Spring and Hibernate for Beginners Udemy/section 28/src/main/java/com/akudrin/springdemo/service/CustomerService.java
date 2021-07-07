63e5b160d033341b2ccfde5f803010173b812c1e
Andrei Kudrin <andreikudrin@gmail.com>
2021-07-07 11:17:01 +0100

    ChadDarby Spring Udemy - Database Web App refactor with Service Layer

---
+++ Spring and Hibernate for Beginners Udemy/section 28/src/main/java/com/akudrin/springdemo/service/CustomerService.java
@@ -0,0 +1,11 @@
+package com.akudrin.springdemo.service;
+
+import java.util.List;
+
+import com.akudrin.springdemo.entity.Customer;
+
+public interface CustomerService {
+
+	public List<Customer> getCustomers();
+
+}
