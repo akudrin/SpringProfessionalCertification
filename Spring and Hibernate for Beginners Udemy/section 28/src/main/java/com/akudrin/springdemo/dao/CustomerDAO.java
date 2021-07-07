2fb3cd3f643244f754aea954232de2c8abd9dc3b
Andrei Kudrin <andreikudrin@gmail.com>
2021-07-07 10:56:16 +0100

    ChadDarby Spring Udemy - Database Web App

---
+++ Spring and Hibernate for Beginners Udemy/section 28/src/main/java/com/akudrin/springdemo/dao/CustomerDAO.java
@@ -0,0 +1,11 @@
+package com.akudrin.springdemo.dao;
+
+import java.util.List;
+
+import com.akudrin.springdemo.entity.Customer;
+
+public interface CustomerDAO {
+
+	public List<Customer> getCustomers();
+
+}
