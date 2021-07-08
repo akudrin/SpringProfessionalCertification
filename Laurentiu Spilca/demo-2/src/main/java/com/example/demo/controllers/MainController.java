e0733ef5343e0f93b2f375710a0548f1fd8f9d1c
Andrei Kudrin <andreikudrin@gmail.com>
2021-07-08 14:35:14 +0100

    LaurentiuSpilca - Spring Security configure()

---
+++ Laurentiu Spilca/demo-2/src/main/java/com/example/demo/controllers/MainController.java
@@ -0,0 +1,24 @@
+package com.example.demo.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+	@GetMapping("/hello")
+	public String hello() {
+		return "hello.html";
+	}
+
+	@GetMapping("/main")
+	public String main() {
+		return "main.html";
+	}
+
+	@GetMapping("/admin")
+	public String admin() {
+		return "admin.html";
+	}
+
+}
